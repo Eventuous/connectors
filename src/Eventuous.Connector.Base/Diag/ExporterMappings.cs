3f950cacf751afa2ca8f0facd51292e0c209d6c5
Alexey Zimarev <alex@zimarev.com>
2022-05-18 16:40:02 +0200

    Log exporters

--- src/Eventuous.Connector.Base/Diag/ExporterMappings.cs
+++ src/Eventuous.Connector.Base/Diag/ExporterMappings.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace Eventuous.Connector.Base.Diag;
 
 public class ExporterMappings<T> {
@@ -9,12 +11,15 @@ public class ExporterMappings<T> {
     }
 
     public void RegisterExporters(T provider, string[]? exporters) {
+        var name = typeof(T).Name;
         if (exporters == null) {
+            Log.Information("No exporters specified for {name}", name);
             return;
         }
 
         foreach (var exporter in exporters) {
             if (_mappings.TryGetValue(exporter, out var addExporter)) {
+                Log.Information("Adding exporter {exporter} for {name}", exporter, name);
                 addExporter(provider);
             }
         }
