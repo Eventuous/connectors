d64bde6435d1b4b8dce4992b6ba8bed3f335cfa3
Alexey Zimarev <alex@zimarev.com>
2022-09-14 16:13:57 +0200

    Added update, insert, and delete many to Mongo

--- src/Eventuous.Connector.EsdbMongo/MongoJsonProjector.cs
+++ src/Eventuous.Connector.EsdbMongo/MongoJsonProjector.cs
@@ -14,9 +14,12 @@ public class MongoJsonProjector : GrpcProjectingProducer<MongoJsonProjector, Mon
         _database = database;
         _log      = log;
 
-        On<InsertOne>((message, token) => InsertOne(message.Stream, message.Message, token));
-        On<UpdateOne>((message, token) => UpdateOne(message.Stream, message.Message, token));
-        On<DeleteOne>((message, token) => DeleteOne(message.Stream, message.Message, token));
+        On<InsertOne>((message,  token) => InsertOne(message.Stream, message.Message, token));
+        On<InsertMany>((message, token) => InsertMany(message.Stream, message.Message, token));
+        On<UpdateOne>((message,  token) => UpdateOne(message.Stream, message.Message, token));
+        On<UpdateMany>((message, token) => UpdateMany(message.Stream, message.Message, token));
+        On<DeleteOne>((message,  token) => DeleteOne(message.Stream, message.Message, token));
+        On<DeleteMany>((message, token) => DeleteMany(message.Stream, message.Message, token));
     }
 
     static readonly ProducerTracingOptions TracingOptions = new() {
@@ -32,6 +35,14 @@ public class MongoJsonProjector : GrpcProjectingProducer<MongoJsonProjector, Mon
             .GetCollection<BsonDocument>(collection)
             .InsertOneAsync(BsonDocument.Parse(insertOne.Document.ToString()), cancellationToken: cancellationToken);
     }
+    
+    Task InsertMany(string collection, InsertMany insertMany, CancellationToken cancellationToken) {
+        _log.LogTrace("Inserting {@Documents}", insertMany);
+
+        return _database
+            .GetCollection<BsonDocument>(collection)
+            .InsertManyAsync(insertMany.Documents.Select(d => BsonDocument.Parse(d.ToString())), cancellationToken: cancellationToken);
+    }
 
     Task UpdateOne(string collection, UpdateOne updateOne, CancellationToken cancellationToken) {
         _log.LogTrace("Updating with {@Update}", updateOne);
@@ -45,12 +56,32 @@ public class MongoJsonProjector : GrpcProjectingProducer<MongoJsonProjector, Mon
             );
     }
 
+    Task UpdateMany(string collection, UpdateMany updateMany, CancellationToken cancellationToken) {
+        _log.LogTrace("Updating with {@Update}", updateMany);
+        
+        return _database.GetCollection<BsonDocument>(collection)
+            .UpdateManyAsync(
+                new JsonFilterDefinition<BsonDocument>(updateMany.Filter.ToString()),
+                new JsonUpdateDefinition<BsonDocument>(updateMany.Update.ToString()),
+                new UpdateOptions { IsUpsert = true },
+                cancellationToken
+            );
+    }
+
     Task DeleteOne(string collection, DeleteOne deleteOne, CancellationToken cancellationToken) {
         _log.LogTrace("Deleting {@Delete}", deleteOne);
 
         return _database
             .GetCollection<BsonDocument>(collection)
             .DeleteOneAsync(new JsonFilterDefinition<BsonDocument>(deleteOne.Filter.ToString()), cancellationToken);
+    }
+
+    Task DeleteMany(string collection, DeleteMany deleteMany, CancellationToken cancellationToken) {
+        _log.LogTrace("Deleting {@Delete}", deleteMany);
+
+        return _database
+            .GetCollection<BsonDocument>(collection)
+            .DeleteManyAsync(new JsonFilterDefinition<BsonDocument>(deleteMany.Filter.ToString()), cancellationToken);
     }
 }
 
