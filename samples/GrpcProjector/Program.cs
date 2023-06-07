6e5657df9b7feb5de539a062ccdd934ae5812247
Alexey Zimarev <alex@zimarev.com>
2023-06-07 09:56:48 +0200

    Add serilog to the sample

--- samples/GrpcProjector/Program.cs
+++ samples/GrpcProjector/Program.cs
@@ -1,7 +1,20 @@
 using GrpcProjector.Services;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Grpc", LogEventLevel.Information)
+    .MinimumLevel.Override("Grpc.Net.Client.Internal.GrpcCall", LogEventLevel.Error)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 
 builder.Services.AddGrpc();
 
@@ -14,6 +27,17 @@ if (builder.Environment.IsDevelopment()) {
 var app = builder.Build();
 app.MapGrpcService<ProjectorService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client");
-var port = Environment.GetEnvironmentVariable("PORT");
-var url  = string.IsNullOrWhiteSpace(port) ? null : $"http://+:{port}";
-app.Run(url);
+
+try {
+    var port = Environment.GetEnvironmentVariable("PORT");
+    var url  = string.IsNullOrWhiteSpace(port) ? null : $"http://+:{port}";
+    app.Run(url);
+    return 0;
+}
+catch (Exception e) {
+    Log.Fatal(e, "Host terminated unexpectedly");
+    return 1;
+}
+finally {
+    Log.CloseAndFlush();
+}
